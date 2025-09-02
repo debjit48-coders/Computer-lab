620d868dd457174b7671cdd3b90cb4a84160aebf
debjit48-coders <gayendebjit48@gmail.com>
2025-09-02 16:18:27 +0530

    Update 24.c

--- day 4/24.c
+++ day 4/24.c
@@ -7,9 +7,10 @@ int main() {
     int c;
     double d;
     printf("size of char:%d\n",sizeof(a));
-    printf("size of float:%d\n",sizeof(a));
-    printf("size of int:%d\n",sizeof(a));
-    printf("size of double:%d\n",sizeof(a));
+    printf("size of float:%d\n",sizeof(b));
+    printf("size of int:%d\n",sizeof(c));
+    printf("size of double:%d\n",sizeof(d));
     
     return 0;
+
 }
